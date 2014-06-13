90b01032092d92be171122665f78176884d3db71
ben <bjwiley2@gmail.com>
2014-06-12 20:05:40 -0400

    Completed counter tests

--- app/count.js
+++ app/count.js
@@ -3,6 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+        
+        var current = start;
+        
+        var iterate = function () {
+            if(current <= end) {
+                console.log(current++);
+                setTimeout(iterate, 100);
+            }
+        };   
+        
+        iterate();
+    
+        return {
+            cancel: function () {
+                current = end + 1;
+            }
+        };    
     }
   };
 });
