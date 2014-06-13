1897d065daa9f04570abc553ccb4cec3987f6eeb
ben <bjwiley2@gmail.com>
2014-06-12 20:16:03 -0400

    Completed flow control test

--- app/count.js
+++ app/count.js
@@ -3,7 +3,6 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
-        
         var current = start;
         
         var iterate = function () {
--- app/flowControl.js
+++ app/flowControl.js
@@ -3,14 +3,29 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     fizzBuzz : function(num) {
-      // write a function that receives a number as its argument;
-      // if the number is divisible by 3, the function should return 'fizz';
-      // if the number is divisible by 5, the function should return 'buzz';
-      // if the number is divisible by 3 and 5, the function should return
-      // 'fizzbuzz';
-      //
-      // otherwise the function should return the number, or false if no number
-      // was provided or the value provided is not a number
+        // write a function that receives a number as its argument;
+        // if the number is divisible by 3, the function should return 'fizz';
+        // if the number is divisible by 5, the function should return 'buzz';
+        // if the number is divisible by 3 and 5, the function should return
+        // 'fizzbuzz';
+        //
+        // otherwise the function should return the number, or false if no number
+        // was provided or the value provided is not a number
+        if(isNaN(num)) {
+            return false;
+        }
+        
+        var fizzOrBuzzOrBoth = [];
+        
+        if(num % 3 === 0) {
+            fizzOrBuzzOrBoth.push('fizz');
+        }
+        
+        if(num % 5 === 0) {
+            fizzOrBuzzOrBoth.push('buzz');
+        }
+        
+        return fizzOrBuzzOrBoth.length > 0 ? fizzOrBuzzOrBoth.join('') : num;
     }
   };
 });
